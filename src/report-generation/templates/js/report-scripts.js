0aa6cc8da26913ba61d433bb22170bf3e11f6fbb
Gilles Fabre <gillesfabre34@gmail.com>
2021-08-27 18:26:19 +0200

    Toggle visibility by metric

--- src/report-generation/templates/js/report-scripts.js
+++ src/report-generation/templates/js/report-scripts.js
@@ -2,8 +2,20 @@ function selectAll(metricName, isSelected, metricNames) {
     console.log('ZZZZZZ', metricName);
     console.log('ZZZZZZ isSelected', isSelected);
     console.log('ZZZZZZssss', metricNames);
+    toggleColorsInArray(metricName, isSelected, metricNames);
+    toggleGloballyVisibility(metricName, isSelected, metricNames);
+}
+
+function toggleColorsInArray(metricName, isSelected, metricNames) {
     for (const name of metricNames) {
         document.getElementById(`all-${name}`).className = 'not-selected';
     }
     document.getElementById(`all-${metricName}`).className = 'selected';
+}
+
+function toggleGloballyVisibility(metricName, isSelected, metricNames) {
+    for (const name of metricNames) {
+        document.getElementById(`div-metric-${name}`).style.display = 'none';
+    }
+    document.getElementById(`div-metric-${metricName}`).style.display = 'block';
 }
