7b2f70d6628ef6fca5482f6f83572a1ddd2a864e
Gilles Fabre <gillesfabre34@gmail.com>
2021-08-27 17:37:28 +0200

    onClick()

---
+++ src/report-generation/templates/js/report-scripts.js
@@ -0,0 +1,6 @@
+function selectAll(metricName) {
+    console.log('ZZZZZZ', metricName);
+    const el = document.getElementById(`all-${metricName}`);
+    el.style.display = 'none';
+    // alert('ok')
+}
