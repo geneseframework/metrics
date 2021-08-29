e5c8e08cdfb2f4bf429c6e5774eea48aa1024a24
Gilles Fabre <gillesfabre34@gmail.com>
2021-08-29 17:32:02 +0200

    Display scatter chart

--- src/report-generation/templates/js/chart-scripts.js
+++ src/report-generation/templates/js/chart-scripts.js
@@ -1,37 +1,33 @@
 
-var chartLeft = document.getElementById('chart-left').getContext("2d");
-// var chartRight = document.getElementById('chart-right').getContext("2d");
-console.log(chartLeft);
-var barCognitive = new Chart(chartLeft, {
-    type: 'bar',
+var chartLeftCanvas = document.getElementById('chart-left').getContext("2d");
+console.log(chartLeftCanvas);
+var barCognitive = new Chart(chartLeftCanvas, {
+    type: 'scatter',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        label: 'Linear Correlation',
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            data: [{
+                x: -10,
+                y: 0
+            }, {
+                x: 0,
+                y: 10
+            }, {
+                x: 10,
+                y: 5
+            }, {
+                x: 0.5,
+                y: 5.5
+            }],
+            backgroundColor: 'rgb(255, 99, 132)'
         }]
     },
     options: {
         scales: {
-            y: {
-                beginAtZero: true
+            x: {
+                type: 'linear',
+                position: 'bottom'
             }
         }
     }
