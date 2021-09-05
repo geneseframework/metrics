6fa055bf5087eb86ce789b6a058d271f976e6e4e
Gilles Fabre <gillesfabre34@gmail.com>
2021-09-05 19:15:07 +0200

    Removed astClasses, AstFunctions, etc.

--- src/core/models/ast-model/ast-file.model.ts
+++ src/core/models/ast-model/ast-file.model.ts
@@ -1,30 +1,20 @@
 import { JsonAstFileInterface } from '../../interfaces/json-ast/json-ast-file.interface';
-// import { AstAbstract } from './ast-abstract.model';
 import { MetricParamValues } from '../../../evaluation/metrics/models/metric-param-value.model';
 import { AstLine } from './ast-line.model';
 import { AstNode } from './ast-node.model';
-import * as chalk from 'chalk';
-// import { AstArrowFunction } from './ast-arrow-function.model';
-// import { AstClass } from './ast-class.model';
-// import { AstCode } from './ast-code.model';
-// import { AstFunction } from './ast-function.model';
 import { JsonAstNodeInterface } from '../../interfaces/json-ast/json-ast-node.interface';
-import { Interval, isInInterval } from '../../../json-ast-to-ast-model/types/interval.type';
 import { AstNodeService } from '../../../json-ast-to-ast-model/services/ast-node.service';
 
 export class AstFile {
 
-
-    astNode: AstNode = undefined;
-    jsonAstNode: JsonAstNodeInterface = undefined;
-    name = undefined;
-    text = '';
-
-
     astLines: AstLine[] = [];
+    astNode: AstNode = undefined;
     jsonAstFile: JsonAstFileInterface = undefined;
+    jsonAstNode: JsonAstNodeInterface = undefined;
     measureValue: number;
     metricParamValues: MetricParamValues = {};
+    name = undefined;
+    text = '';
 
     constructor(jsonAstFile: JsonAstFileInterface) {
         this.jsonAstFile = jsonAstFile;
@@ -76,6 +66,7 @@ export class AstFile {
         }
     }
 
+    // TODO: re-implement ?
     private setCallBacks(): void {
         if (!this.astNode.isFunc) {
             return;
